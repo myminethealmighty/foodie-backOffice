d06b22ce3f162d7d3a6739a1f1fbe55cdcbd5fe8
Min Khant Thar <124900463+myminethealmighty@users.noreply.github.com>
2023-11-09 21:36:57 +0630

    Updating Opacity for disabled menu categories

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,41 +1,45 @@
-import { Paper, Typography } from "@mui/material";
+import { Paper, Tooltip, Typography } from "@mui/material";
 import Link from "next/link";
 import { ReactNode } from "react";
 
 interface Props {
   icon: ReactNode;
   title: string;
+  isAvailable?: boolean;
   href?: string;
   subtitle?: string;
 }
 
-const ItemCard = ({ icon, title, href, subtitle }: Props) => {
+const ItemCard = ({ icon, title, isAvailable, href, subtitle }: Props) => {
   if (href) {
     return (
       <Link href={href} style={{ textDecoration: "none", color: "#000000" }}>
-        <Paper
-          elevation={2}
-          sx={{
-            width: 170,
-            height: 170,
-            p: 2,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            m: 2,
-          }}
-        >
-          {icon}
-          <Typography sx={{ color: "#4C4C6D", fontWeight: "700" }}>
-            {title}
-          </Typography>
-          {subtitle && (
-            <Typography sx={{ color: "#4C4C6D", fontSize: 14 }}>
-              {subtitle}
+        <Tooltip arrow title={isAvailable === false ? "Unavailable" : ""}>
+          <Paper
+            elevation={2}
+            sx={{
+              width: 170,
+              height: 170,
+              p: 2,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              m: 2,
+              opacity: isAvailable === false ? 0.4 : 1,
+            }}
+          >
+            {icon}
+            <Typography sx={{ color: "#4C4C6D", fontWeight: "700" }}>
+              {title}
             </Typography>
-          )}
-        </Paper>
+            {subtitle && (
+              <Typography sx={{ color: "#4C4C6D", fontSize: 14 }}>
+                {subtitle}
+              </Typography>
+            )}
+          </Paper>
+        </Tooltip>
       </Link>
     );
   }
--- src/pages/backoffice/menu-categories/index.tsx
+++ src/pages/backoffice/menu-categories/index.tsx
@@ -1,13 +1,16 @@
 import ItemCard from "@/components/ItemCard";
 import NewMenuCategory from "@/components/NewMenuCategory";
 import { useAppSelector } from "@/store/hook";
-import CategoryIcon from '@mui/icons-material/Category';
+import CategoryIcon from "@mui/icons-material/Category";
 import { Box, Button } from "@mui/material";
 import { useState } from "react";
 
 const MenuCategoriesPage = () => {
   const [open, setOpen] = useState(false);
   const menuCategories = useAppSelector((state) => state.menuCategory.items);
+  const disabledLocationMenuCategories = useAppSelector(
+    (state) => state.disabledLocationMenuCategorySlice.items
+  );
   return (
     <Box>
       <Box
@@ -20,15 +23,25 @@ const MenuCategoriesPage = () => {
           + New
         </Button>
       </Box>
-      <Box sx={{display:'flex', flexWrap:'wrap'}}>
-        {menuCategories.map((item) => (
-          <ItemCard
-          href={`/backoffice/menu-categories/${item.id}`}
-          key={item.id}
-          title={item.name}
-          icon={<CategoryIcon />}
-        />
-        ))}
+      <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+        {menuCategories.map((item) => {
+          const exit = disabledLocationMenuCategories.find(
+            (disabledLocationMenuCategory) =>
+              disabledLocationMenuCategory.locationId ===
+                Number(localStorage.getItem("selectedLocationId")) &&
+              disabledLocationMenuCategory.menuCategoryId === item.id
+          );
+          const isAvailable = exit ? false : true;
+          return (
+            <ItemCard
+              href={`/backoffice/menu-categories/${item.id}`}
+              key={item.id}
+              title={item.name}
+              icon={<CategoryIcon />}
+              isAvailable={isAvailable}
+            />
+          );
+        })}
       </Box>
       <NewMenuCategory open={open} setOpen={setOpen} />
     </Box>
